4a4e1a9748aadd715170ea53dd57d9eca9214af3
Hansimov <591172499@qq.com>
2023-08-11 17:26:38 +0800

    [Feature] New calculators: distribute_weights(), weighted_avg(), closest_category(), and fix indent typo of get_neighbors()

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,4 +1,5 @@
 from itertools import chain
+import numpy as np
 
 
 def flatten(nested_list):
@@ -68,15 +69,71 @@ def get_neighbors(i, elements, n=5, include_i=True):
         else:
             pass
 
-    if include_i:
-        neighbor_idxs = list(range(i - n_before, i + 1 + n_after))
-    else:
-        neighbor_idxs = list(range(i - n_before, i)) + list(
-            range(i + 1, i + 1 + n_after)
-        )
+        if include_i:
+            neighbor_idxs = list(range(i - n_before, i + 1 + n_after))
+        else:
+            neighbor_idxs = list(range(i - n_before, i)) + list(
+                range(i + 1, i + 1 + n_after)
+            )
+
     neighbor_elements = [elements[idx] for idx in neighbor_idxs]
     return neighbor_idxs, neighbor_elements
 
 
 def weighted_sum(values, weights):
     return sum([v * w for v, w in zip(values, weights)])
+
+
+def distribute_weights(weights, distribution=None):
+    n = len(weights)
+
+    if distribution == "normal":
+        seq = np.linspace(-n / 2, n / 2, n)
+        scales = np.exp(-(seq**2) / (n**2))
+        weights = weights * scales
+    elif distribution == "linear":
+        if n % 2 == 0:
+            scales = np.concatenate(
+                (
+                    np.linspace(0.5, 1, n // 2),
+                    np.linspace(1, 0.5, n // 2),
+                )
+            )
+        else:
+            scales = np.concatenate(
+                (
+                    np.linspace(0.5, 1, n // 2 + 1),
+                    np.linspace(0.5, 1, n // 2 + 1)[::-1][1:],
+                )
+            )
+        weights = weights * scales
+    else:
+        pass
+
+    weights /= np.sum(weights)
+
+    return weights
+
+
+def weighted_avg(values, weights):
+    return np.average(values, weights=weights)
+
+
+def closest_category(value, category_enums, dist_func=None, prefer_new=True):
+    def abs_diff(x, y):
+        return abs(x - y)
+
+    if dist_func is None:
+        dist_func = abs_diff
+
+    distances = [dist_func(value, category) for category in category_enums]
+
+    min_dist_idx = 0
+    for i, dist in enumerate(distances):
+        if prefer_new:
+            if dist <= distances[min_dist_idx]:
+                min_dist_idx = i
+        else:
+            if dist < distances[min_dist_idx]:
+                min_dist_idx = i
+    return category_enums[min_dist_idx]
