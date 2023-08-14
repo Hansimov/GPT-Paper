d4c5fc230a5ff17d1414c5a709b5e5bd2fd403c9
Hansimov <591172499@qq.com>
2023-08-15 00:37:09 +0800

    [Enhance] Fix import issues

--- tests/install_detectron2.py
+++ tests/install_detectron2.py
@@ -12,9 +12,16 @@ detectron2_path = Path(__file__).parents[2] / "detectron2"
 dist = distutils.core.run_setup(detectron2_path / "setup.py")
 
 install_packages = dist.install_requires
-install_packages_str = " ".join([f"'{x}'" for x in dist.install_requires])
+# install_packages_str = " ".join([f"'{x}'" for x in dist.install_requires])
 print(install_packages)
 
-subprocess.run(f"pip install {install_packages_str}", shell=True)
+# "fvcore>=0.1.5,<0.1.6",  # required like this to make it pip installable
+# "iopath>=0.1.7,<0.1.10",
+
+for install_package in install_packages:
+    print(f"Installing: {install_package}")
+    subprocess.run(f"pip install {install_package}", shell=True)
+    # subprocess.run(f"pip install {install_packages_str}", shell=True)
+
 
 sys.path.insert(0, os.path.abspath(str(detectron2_path)))
