2475e2fa0f5579f64b7edfecd301870d01a458cf
Hansimov <591172499@qq.com>
2023-08-15 16:02:29 +0800

    [Feature] New utils.envs: Setup detectron2, unilm, and dit

--- utils/envs.py
+++ utils/envs.py
@@ -1,11 +1,11 @@
 import ctypes
 import json
 import os
+import re
 from ctypes.util import find_library
 from pathlib import Path
-from utils.logger import Logger
-
-logger = Logger().logger
+from termcolor import colored
+from utils.logger import logger, shell_cmd
 
 
 def init_os_envs(secrets=True, apis=[], set_proxy=True, cuda_device=None):
@@ -49,6 +49,88 @@ def check_camelot_dependencies():
         "".join(("gsdll", str(ctypes.sizeof(ctypes.c_voidp) * 8), ".dll"))
     )
     logger.info(f"GhostScript: {gs_res}")
+
+
+repo_path = Path(__file__).parents[1]
+repo_parent_path = repo_path.parent
+
+
+def setup_envs_of_detectron2(clone_repo=True, install_dependencies=True):
+    # git clone repo of detectron2
+    detectron2_path = repo_parent_path / "detectron2"
+
+    if clone_repo and not detectron2_path.exists():
+        shell_cmd(
+            f"git clone https://github.com/facebookresearch/detectron2.git {str(detectron2_path)}"
+        )
+
+    # install dependencies of detectron2
+    if install_dependencies:
+        logger.warning(
+            colored("See README.md to install `detectron2` dependencies.", "light_red")
+        )
+
+        logger.info(
+            colored("Installing required packages of detectron2:", "light_magenta")
+        )
+        shell_cmd(
+            'pip install "Pillow>=7.1" matplotlib "pycocotools>=2.0.2" "termcolor>=1.1" "yacs>=0.1.8" tabulate cloudpickle "tqdm>4.29.0" tensorboard "fvcore>=0.1.5,<0.1.6" "iopath>=0.1.7,<0.1.10" "omegaconf>=2.1,<2.4" "hydra-core>=1.1" black packaging'
+        )
+
+        logger.info(
+            colored("Installing extra packages of detectron2:", "light_magenta")
+        )
+        shell_cmd(
+            'pip install fairscale timm "scipy>1.5.1" shapely "pygments>=2.2" "psutil" "panopticapi @ https://github.com/cocodataset/panopticapi/archive/master.zip"'
+        )
+
+
+def setup_envs_of_unilm():
+    # git clone repo of unilm
+    unilm_path = repo_parent_path / "unilm"
+    if not unilm_path.exists():
+        shell_cmd(f"git clone https://github.com/microsoft/unilm.git {str(unilm_path)}")
+
+    # patch `data_structure.py` in dit
+    logger.info(colored("Patching `data_structure.py` in dit ...", "light_magenta"))
+    orignial_str = "from collections import Iterable"
+    replaced_str = "from collections.abc import Iterable"
+    data_structure_py = (
+        unilm_path
+        / "dit"
+        / "object_detection"
+        / "ditod"
+        / "table_evaluation"
+        / "data_structure.py"
+    )
+
+    with open(data_structure_py, "r") as rf:
+        original_text = rf.read()
+        replaced_text = re.sub(orignial_str, replaced_str, original_text)
+
+    with open(data_structure_py, "w") as wf:
+        wf.write(replaced_text)
+
+
+def download_publaynet_dit_b_cascade_pth(
+    http_proxy="http://localhost:11111",
+    url="https://layoutlm.blob.core.windows.net/dit/dit-fts/publaynet_dit-b_cascade.pth?sv=2022-11-02&ss=b&srt=o&sp=r&se=2033-06-08T16:48:15Z&st=2023-06-08T08:48:15Z&spr=https&sig=a9VXrihTzbWyVfaIDlIT1Z0FoR1073VB0RLQUMuudD4%3D",
+    output_path=repo_path / "configs" / "publaynet_dit-b_cascade.pth",
+    overwrite=False,
+):
+    if http_proxy:
+        proxy_str = f" --proxy '{http_proxy}' "
+    else:
+        proxy_str = ""
+    if overwrite or not output_path.exists():
+        logger.info(colored(f"Downloading {str(output_path)} ...", "light_magenta"))
+        shell_cmd(f"curl {proxy_str} -LJ -o {output_path} '{url}'")
+
+
+def setup_envs_of_dit():
+    setup_envs_of_detectron2()
+    setup_envs_of_unilm()
+    download_publaynet_dit_b_cascade_pth()
 
 
 if __name__ == "__main__":
