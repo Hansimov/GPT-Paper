604ada1a6a40f10e42aabb5629e0fcdd59f8a26e
Hansimov <591172499@qq.com>
2023-08-17 02:22:26 +0800

    [Feature] Enable OCR with tesseract, and drop TrOCR as it only supports singe line

--- utils/text_extractor.py
+++ utils/text_extractor.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from PIL import Image
-from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+
 from utils.logger import logger
 from utils.envs import init_os_envs
 import platform
+import pytesseract
 
 if platform.system() == "Windows":
     init_os_envs(apis=["huggingface"], cuda_device=0)
@@ -13,24 +14,23 @@ else:
 
 class TextExtractor:
     def __init__(self):
-        self.model_name = "microsoft/trocr-large-printed"
+        pass
 
     def run(self):
         image = Image.open(
             Path(__file__).parents[1]
             / "pdfs"
-            / "Exploring pathological signatures for predicting the recurrence of early-stage hepatocellular carcinoma based on deep learning/crops/page_2/region_1_text_99.9.png"
+            / "Exploring pathological signatures for predicting the recurrence of early-stage hepatocellular carcinoma based on deep learning"
+            / "crops"
+            / "page_1"
+            # / "region_1_text_99.95.png"
+            # / "region_5_text_99.31.png"
+            / "region_6_text_98.92.png"
+            # / "region_14_text_13.72.png"
         )
-        processor = TrOCRProcessor.from_pretrained(self.model_name)
-        model = VisionEncoderDecoderModel.from_pretrained(self.model_name)
-        pixel_values = processor(images=image, return_tensors="pt").pixel_values
-
-        generated_ids = model.generate(pixel_values)
-        generated_text = processor.batch_decode(
-            generated_ids, skip_special_tokens=True
-        )[0]
+        text = pytesseract.image_to_string(image)
         logger.note("Text extracted from image:")
-        logger.success(generated_text)
+        logger.success(text)
 
 
 if __name__ == "__main__":
