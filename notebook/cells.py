ce893ad7f3cd887625d19fed157642195c20a7a7
Hansimov <591172499@qq.com>
2023-09-03 12:34:34 +0800

    [Feature] New util: get_cell_index()

--- notebook/cells.py
+++ notebook/cells.py
@@ -1,16 +1,26 @@
-import json
 import nbformat
 import ipynbname
 
 
-def read_cells(notebook_path=None, cell_types=["markdown", "code", "raw"]):
+def get_notebook_cells(notebook_path=None, cell_types=["markdown", "code", "raw"]):
     if not notebook_path:
         notebook_path = ipynbname.path()
 
     with open(notebook_path, "r", encoding="utf-8") as rf:
         nb = nbformat.read(rf, as_version=4)
 
-    cells = [
-        (i, cell) for i, cell in enumerate(nb.cells) if cell["cell_type"] in cell_types
-    ]
+    cells = [cell for i, cell in enumerate(nb.cells) if cell["cell_type"] in cell_types]
     return cells
+
+
+def get_cell_id():
+    cell_id = get_ipython().get_parent()["metadata"]["cellId"]
+    return cell_id
+
+
+def get_cell_index():
+    cell_id = get_cell_id()
+    cells = get_notebook_cells()
+    for idx, cell in enumerate(cells):
+        if cell["id"] == cell_id:
+            return idx
