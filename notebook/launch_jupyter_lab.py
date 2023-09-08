b57d06dde8f7a7fb65aa2a5dbfacd2c72a0f54df
Hansimov <591172499@qq.com>
2023-09-08 14:10:01 +0800

    [Config] Customize token for JupyterLabLauncher

--- notebook/launch_jupyter_lab.py
+++ notebook/launch_jupyter_lab.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 from utils.logger import shell_cmd
@@ -9,18 +10,24 @@ class JupyterLabLauncher:
         self.ipynb_path = Path(__file__).parent / "review_writer.ipynb"
 
     def init_envs(self):
+        with open(Path(__file__).parents[1] / "secrets.json", "r") as rf:
+            secrets = json.load(rf)
+        jupyter_token = secrets["jupyter_token"]
         self.envs = {
             "JUPYTER_CONFIG_DIR": str(Path(__file__).parent),
+            "JUPYTER_TOKEN": jupyter_token,
         }
         for env in self.envs:
             os.environ[env] = self.envs[env]
+        self.ip = "0.0.0.0"
+        self.port = "28888"
 
     def generate_config(self):
         shell_cmd("jupyter lab --generate-config")
 
     def run(self):
         # shell_cmd(f"jupyter lab {self.ipynb_path}")
-        shell_cmd(f"jupyter lab --ip=0.0.0.0 --port=28888 {self.ipynb_path}")
+        shell_cmd(f"jupyter lab --ip={self.ip} --port={self.port} {self.ipynb_path}")
 
 
 if __name__ == "__main__":
