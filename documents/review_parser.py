33692df501eb5120b30c036f24b1f2239dc32be1
Hansimov <591172499@qq.com>
2023-09-08 18:55:50 +0800

    [Feature] (WIP) Construct hierarchy for section nodes

--- documents/review_parser.py
+++ documents/review_parser.py
@@ -5,6 +5,14 @@ import pprint
 from pathlib import Path
 
 
+def calc_level_depth(level, level_0_depth=0):
+    level = re.sub(r"\.$", "", level)
+    level_depth = len(level.split("."))
+    if str(level) == "0":
+        level_depth = level_0_depth
+    return level_depth
+
+
 class ReviewParser:
     pdf_root = Path(__file__).parents[1] / "pdfs"
 
@@ -23,8 +31,8 @@ class ReviewParser:
         return self.outline
 
     def dump_outline(self):
-        with open(self.review_outline_json_path, "w", encoding="utf-8") as rf:
-            json.dump(self.outline, rf, indent=2, ensure_ascii=False)
+        with open(self.review_outline_json_path, "w", encoding="utf-8") as wf:
+            json.dump(self.outline, wf, indent=2, ensure_ascii=False)
         return self.review_outline_json_path
 
     def load_sections(self):
@@ -37,7 +45,10 @@ class ReviewParser:
             { "idx": 0, "level": "0",   "title": "...", "intro": "..." },
             { "idx": 1, "level": "1",   "title": "...", "intro": "..." },
             { "idx": 2, "level": "1.1", "title": "...", "intro": "..." },
-            { "idx": 3, "level": "1.2", "title": "...", "intro": "..." },
+            { "idx": 3, "level": "1.1.1", "title": "...", "intro": "..." },
+            { "idx": 4, "level": "1.1.2", "title": "...", "intro": "..." },
+            { "idx": 5, "level": "1.2", "title": "...", "intro": "..." },
+            ...
         ]
         ```
         """
@@ -46,21 +57,17 @@ class ReviewParser:
         return self.sections
 
     def dump_sections(self):
-        with open(self.review_sections_json_path, "w", encoding="utf-8") as rf:
-            json.dump(self.sections, rf, indent=2, ensure_ascii=False)
+        with open(self.review_sections_json_path, "w", encoding="utf-8") as wf:
+            json.dump(self.sections, wf, indent=2, ensure_ascii=False)
         return self.review_sections_json_path
 
-    def calc_level_depth(self, level):
-        level = re.sub(r"\.$", "", level)
-        level_depth = len(level.split("."))
-        if str(level) == "0":
-            level_depth = 0
-        return level_depth
+    def calc_section_level_depth(self, section, level_0_depth=0):
+        return calc_level_depth(section["level"], level_0_depth=level_0_depth)
 
     def get_sections_at_depth(self, depth=1, op=operator.eq, contain_main_title=False):
         sections_at_depth = []
         for section in self.sections:
-            section_level_depth = self.calc_level_depth(section["level"])
+            section_level_depth = self.calc_section_level_depth(section)
             if section_level_depth == 0 and not contain_main_title:
                 continue
             if op(section_level_depth, depth):
@@ -90,6 +97,96 @@ class ReviewParser:
             depth=depth, op=op, contain_main_title=contain_main_title
         )
         pprint.pprint(sections_at_depth)
+
+    def reorder_sections_idxs(self):
+        self.sections.sort(key=lambda x: x["idx"])
+        section_idx = self.sections[0]["idx"]
+        for section in self.sections:
+            section["idx"] = section_idx
+            section_idx += 1
+
+    def get_last_section_in_hierarchy(self, hierarchical_sections):
+        last_section = hierarchical_sections[-1]
+        while last_section["children"]:
+            last_section = last_section["children"][-1]
+        return last_section
+
+    def construct_hierarchical_sections(self):
+        """
+        Return:
+        ```json
+        [
+            {
+                "idx": 0, "level": "0", "title": "...", "intro": "...", "children": []
+            },
+            {
+                "idx": 1, "level": "1",   "title": "...", "intro": "...",
+                "children": [
+                    { "idx": 2, "level": "1.1", "title": "...", "intro": "..."
+                        "children": [
+                            {"idx": 3, "level": "1.1.1", "title": "...", "intro": "...",
+                                "children": [],
+                            },
+                            {"idx": 4, "level": "1.1.2", "title": "...", "intro": "...",
+                                "children": [],
+                            },
+                        ],
+                    },
+                    { "idx": 5, "level": "1.2", "title": "...", "intro": "...",
+                        "children": [...],
+                    },
+                    ...
+                ]
+            },
+            {
+                "idx": 6, "level": "2",   "title": "...", "intro": "...",
+                "children": [...]
+            },
+            ...
+        ]
+        ```
+        """
+
+        self.hierarchical_sections = []
+
+        for section in self.sections:
+            section["children"] = []
+
+        section_root = SectionNode()
+        prev_section_node = section_root
+        for section in self.sections:
+            section_node = SectionNode(section=section, parent=prev_section_node)
+            if section_node.depth == prev_section_node.depth:
+                prev_section_node.parent.children.append(section_node)
+            elif section_node.depth > prev_section_node.depth:
+                prev_section_node.children.append(section_node)
+            else:
+                parent = prev_section_node.get_latest_node_with_depth(
+                    section_node.depth
+                ).parent
+                parent.children.append(section_node)
+            prev_section_node = section_node
+
+
+class SectionNode:
+    def __init__(self, section=None, parent=None, children=[]):
+        self.section = section
+        self.parent = parent
+        self.children = children
+
+        if self.section is None:
+            self.depth = 0
+        else:
+            self.title = section["title"]
+            self.intro = section["intro"]
+            self.level = section["level"]
+            self.depth = calc_level_depth(section["level"])
+
+    def get_latest_node_with_depth(self, depth):
+        latest_node = self
+        while latest_node.depth > depth:
+            latest_node = latest_node.parent
+        return latest_node
 
 
 if __name__ == "__main__":
