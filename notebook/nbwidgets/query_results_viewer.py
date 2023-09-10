f52032dc5ced061c4219f2d064650b81580f346f
Hansimov <591172499@qq.com>
2023-09-11 02:36:47 +0800

    [Feature] QueryResultsViewer: Style with normalized scores, and ordered list for pdf name

--- notebook/nbwidgets/query_results_viewer.py
+++ notebook/nbwidgets/query_results_viewer.py
@@ -8,21 +8,43 @@ class QueryResultsViewer:
         self.create_widgets()
 
     def queries_to_html(self):
+        region_scores = [
+            region["score"]
+            for query_result in self.queries
+            for region in query_result["regions"]
+        ]
+
+        def normalize_score(score):
+            return (score - min(region_scores)) / (
+                max(region_scores) - min(region_scores)
+            )
+
         html_str = ""
         for query_result in self.queries:
             pdf_name = query_result["pdf_name"]
             pdf_name_html = f"<h3>{pdf_name}</h3>\n"
-            text_list_html = "\n".join(
-                [f"<li>{region['text']}</li>" for region in query_result["regions"]]
-            )
-            texts_html = f"<ol>\n{text_list_html}\n</ol>"
-            html_str += f"{pdf_name_html}\n{texts_html}"
-        html_str = f"<div class='query_results'>{html_str}</div>"
+
+            region_texts_html = ""
+            for region in query_result["regions"]:
+                normalized_region_score = normalize_score(region["score"])
+                region_text_style = (
+                    f"background-color: rgba(0, 200, 0, {normalized_region_score});"
+                )
+                region_text_html = (
+                    f"<li style='{region_text_style}'>{region['text']}</li>\n"
+                )
+                region_texts_html += region_text_html
+
+            region_texts_html = f"<ol>\n{region_texts_html}\n</ol>"
+            html_str += f"<li>{pdf_name_html}\n{region_texts_html}</li>"
+
+        html_str = f"<div class='query_results'><ol>{html_str}</ol></div>"
         style_str = f"""
         <style>
             .query_results {{
-                height: 400px;
-                overflow-y:auto
+                max-height: 900px;
+                overflow-y: auto;
+                border: 1px solid gray;
             }}
         </style>
         """
