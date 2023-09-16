90b01e71540f8d3f97d26e6981dbe01e72511559
Hansimov <591172499@qq.com>
2023-09-17 03:27:24 +0800

    [Feature] ConversationViewer: Pop message button

--- notebook/nbwidgets/conversation_viewer.py
+++ notebook/nbwidgets/conversation_viewer.py
@@ -5,6 +5,8 @@ from nbwidgets.message_node import MessageNode, MessageChain, MessageTree
 from time import sleep
 from datetime import datetime
 from agents.openai import OpenAIAgent
+from functools import partial
+from pprint import pprint
 
 
 class ConversationViewer:
@@ -14,10 +16,17 @@ class ConversationViewer:
         self.append_messages(messages)
         self.display()
 
-    def pop_message(self):
-        if len(self.message_viewers) == 0:
-            return None
-        return self.message_viewers.pop()
+    def pop_message(self, button=None, display=False, pop_count=1):
+        popped_messages = []
+        for _ in range(pop_count):
+            if len(self.message_viewers) == 0:
+                break
+            popped_message = self.message_viewers.pop()
+            pprint(popped_message.message_node.to_dict())
+            popped_messages.append(popped_message)
+        if display:
+            self.display()
+        return popped_messages
 
     def append_messages(self, messages):
         if messages is None:
@@ -71,6 +80,18 @@ class ConversationViewer:
         )
         self.regenerate_button.on_click(self.regenerate_message)
 
+        self.pop_message_button = widgets.Button(
+            description="Pop", layout=widgets.Layout(width="auto")
+        )
+        self.pop_message_button.on_click(
+            partial(self.pop_message, display=True, pop_count=2)
+        )
+
+        self.stop_button = widgets.Button(
+            description="Stop", layout=widgets.Layout(width="auto")
+        )
+        self.stop_button.on_click(self.stop_chat)
+
         self.model_html_widget = widgets.HTML(value="<div>model: </div>")
         self.available_models = [
             "poe-gpt-3.5-turbo-16k",
@@ -85,6 +106,8 @@ class ConversationViewer:
         self.buttons_box.children = [
             self.submit_button,
             self.regenerate_button,
+            self.stop_button,
+            self.pop_message_button,
             self.model_html_widget,
             self.model_dropdown,
         ]
@@ -146,3 +169,9 @@ class ConversationViewer:
             last_message_viewer.text_widget.value
         )
         last_message_viewer.message_node.content = response_content
+        last_message_viewer.sync_text_to_html()
+
+    def stop_chat(self, button=None):
+        self.stop_button.style.button_color = "orange"
+        # self.pop_message()
+        self.stop_button.style.button_color = "darkgreen"
