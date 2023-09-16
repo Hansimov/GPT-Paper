873e8b7c0203025339f780034f7d879854de1311
Hansimov <591172499@qq.com>
2023-09-17 06:10:32 +0800

    [Doc] Add converted documents

---
+++ tests/test_md2docx.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import pypandoc
+
+md_path = (
+    Path(__file__).parents[1] / "pdfs" / "cancer_review" / "_results" / "review_v1.md"
+)
+
+docx_path = md_path.with_suffix("").with_suffix(".docx")
+
+pypandoc.convert_file(md_path, "docx", format="md", outputfile=docx_path)
