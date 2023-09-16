17a46e06f666371f22c5f47fb0aec04425ca490b
Hansimov <591172499@qq.com>
2023-09-16 21:10:33 +0800

    [Feature] ConversationViewer: Add buttons, and enhance submit logics

--- notebook/nbwidgets/conversation_viewer.py
+++ notebook/nbwidgets/conversation_viewer.py
@@ -9,25 +9,34 @@ class ConversationViewer:
         self.output_viewers = []
 
     def create_user_input_widget(self):
-        # self.user_input_viewer = OutputViewer(
-        #     output="?", id="user_input", editable=True
-        # )
         self.user_input_viewer = UserInputViewer()
-        # self.user_input_viewer.apply_style(
-        #     f"""
-        #         background-color: rgba(100, 100, 100, 0.5);
-        #         padding: 8px;
-        #     """
-        # )
-        self.user_input_button = widgets.Button(description="Submit")
-        self.user_input_button.on_click(self.submit_user_input)
-        self.user_input_widget = widgets.VBox(
-            [self.user_input_viewer.output_widget, self.user_input_button]
+
+    def create_buttons(self):
+        self.buttons_box = widgets.HBox()
+        self.submit_button = widgets.Button(
+            description="Submit", layout=widgets.Layout(width="auto")
         )
+        self.submit_button.on_click(self.submit_user_input)
+        self.model_html_widget = widgets.HTML(value="<div>model: </div>")
+        self.model_dropdown = widgets.Dropdown(
+            options=["poe-gpt-3.5-turbo", "poe-gpt-3.5-turbo-16k", "poe-gpt-4"],
+            value="poe-gpt-3.5-turbo-16k",
+            layout=widgets.Layout(width="auto"),
+        )
+        self.buttons_box.children = [
+            self.submit_button,
+            self.model_html_widget,
+            self.model_dropdown,
+        ]
 
     def submit_user_input(self, button=None):
-        self.user_input_viewer.on_submit()
-        # self.user_input_viewer.switch_widget_mode(mode="html")
+        self.submit_button.style.button_color = "orange"
+        user_input_content = self.user_input_viewer.on_submit()
+        if user_input_content:
+            self.submit_button.style.button_color = "darkgreen"
+        else:
+            self.submit_button.style.button_color = None
+
     def construct_widgets_from_messages(self):
         for message in self.messages:
             role = message["role"]
@@ -49,10 +58,11 @@ class ConversationViewer:
             self.output_viewers.append(output_viewer)
 
         self.create_user_input_widget()
-        self.container = widgets.VBox(
-            [output_viewer.widget for output_viewer in self.output_viewers]
-            + [self.user_input_widget]
-        )
+        self.create_buttons()
+        self.widgets = [
+            output_viewer.widget for output_viewer in self.output_viewers
+        ] + [self.user_input_viewer.output_widget, self.buttons_box]
+        self.container = widgets.VBox(self.widgets)
 
     def display(self):
         display(self.container)
