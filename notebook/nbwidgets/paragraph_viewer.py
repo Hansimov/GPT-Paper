9a6569187f3c4225c87a470d6091e9a25cbb63f7
Hansimov <591172499@qq.com>
2023-09-21 19:34:51 +0800

    [Feature] ParagraphViewer: Split to sentences, and refactor widgets

--- notebook/nbwidgets/paragraph_viewer.py
+++ notebook/nbwidgets/paragraph_viewer.py
@@ -1,21 +1,60 @@
 import ipywidgets as widgets
 from nbwidgets.message_viewer import MessageViewer, MessageNode
 from IPython.display import display
+from utils.tokenizer import SentenceTokenizer
 
 
 class ParagraphViewer:
     def __init__(self):
         self.create_widgets()
 
-    def create_text_editor(self):
+    def create_output_widget(self):
+        self.output_widget = widgets.Output(layout=widgets.Layout())
+
+    def create_text_viewer(self):
         self.text_viewer = MessageViewer(
             MessageNode(role="input"), placeholder="Paragraph"
         )
 
+    def create_result_viewer(self):
+        self.result_viewer = MessageViewer(MessageNode(role="other"))
+
+    def create_buttons(self):
+        self.split_to_sentences_button = widgets.Button(
+            description="Split paragraph",
+            layout=widgets.Layout(width="auto"),
+        )
+        self.split_to_sentences_button.on_click(self.split_to_sentences)
+
+        self.buttons_box = widgets.HBox([self.split_to_sentences_button])
+
+    def split_to_sentences(self, button=None):
+        self.split_to_sentences_button.style.button_color = "orange"
+        paragraph_text = self.text_viewer.text_widget.value
+        if not paragraph_text.strip():
+            self.split_to_sentences_button.style.button_color = None
+            return
+        self.sentence_tokenizer = SentenceTokenizer()
+        sentences = self.sentence_tokenizer.text_to_sentences(paragraph_text)
+        self.result_viewer.text_widget.value = "\n".join(
+            [f"{idx+1}. {sentence}" for idx, sentence in enumerate(sentences)]
+        )
+        self.result_viewer.sync_text_to_html()
+        self.split_to_sentences_button.style.button_color = "green"
+
+    def send_to_query(self, button=None):
+        pass
+
     def create_widgets(self):
-        self.create_text_editor()
-        self.output_widget = self.text_viewer.output_widget
-        self.widget = self.text_viewer.widget
+        self.create_output_widget()
+        self.create_text_viewer()
+        self.create_result_viewer()
+        self.create_buttons()
 
     def display(self):
-        self.text_viewer.display()
+        with self.output_widget:
+            for viewer in [self.text_viewer, self.result_viewer]:
+                display(viewer.output_widget)
+                viewer.sync_text_to_html()
+                viewer.display()
+        display(self.buttons_box)
