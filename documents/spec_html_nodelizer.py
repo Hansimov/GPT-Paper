211df67bf0bdd6fd4588f655654e4997c4aa419e
Hansimov <591172499@qq.com>
2023-10-19 15:33:38 +0800

    [Feature] SectionNode: Connect Nodes by sections

--- documents/spec_html_nodelizer.py
+++ documents/spec_html_nodelizer.py
@@ -29,30 +29,20 @@ class Node:
         return self.parent_element
 
 
-class ElementNodelizer:
-    def __init__(self, element):
-        node = None
-        tag = element.name
-        class_str = " ".join(element.get("class", []))
+class SectionNode(Node):
+    def parse_element(self):
+        self.type = "section"
 
-        if tag == "script":
-            node = JavascriptNode(element)
-        if tag in ["h1", "h2", "h3", "h4", "h5", "h6"]:
-            node = HeaderNode(element)
-        if class_str == "table" or tag == "table":
-            node = TableNode(element)
-        if class_str == "figure":
-            node = FigureNode(element)
-        if class_str == "sourceCode" or tag == "pre":
-            node = CodeNode(element)
-        if tag in ["p"]:
-            node = TextNode(element)
-        if tag in ["ul", "ol"]:
-            node = ListNode(element)
-        if tag in ["hr"]:
-            node = SepNode(element)
+    def get_header_node(self):
+        for child in self.children:
+            if child.type == "header":
+                return child
 
-        self.node = node
+    def get_section_level(self):
+        pass
+
+    def get_children(self):
+        pass
 
 
 class JavascriptNode(Node):
@@ -64,31 +54,36 @@ class TextNode(Node):
     def parse_element(self):
         self.type = "text"
         self.get_text()
+        self.get_full_text()
 
     def get_text(self):
         self.text = self.element.text.strip()
-        if self.class_str == "author":
-            print(f"{self.class_str}: {self.text}")
         return self.text
+
+    def get_full_text(self):
+        if self.class_str:
+            self.full_text = f"{self.class_str}: {self.text}"
+        else:
+            self.full_text = self.text
+        return self.full_text
 
 
 class HeaderNode(Node):
     def parse_element(self):
         self.type = "header"
-
-        self.get_header_level()
-        self.get_header_number()
-        self.get_header_text()
+        self.get_level()
+        self.get_number()
+        self.get_text()
         self.get_full_text()
         self.get_indented_full_text()
 
         # print(self.indented_full_text)
 
-    def get_header_level(self):
-        self.header_level = int(self.tag[-1])
-        return self.header_level
+    def get_level(self):
+        self.level = int(self.tag[-1])
+        return self.level
 
-    def get_header_number(self):
+    def get_number(self):
         span_element = self.element.find("span")
 
         if span_element:
@@ -99,18 +94,18 @@ class HeaderNode(Node):
 
         return self.header_number
 
-    def get_header_text(self):
-        self.header_text = self.element.text.strip()
-        return self.header_text
+    def get_text(self):
+        self.text = self.element.text.strip()
+        return self.text
 
     def get_full_text(self):
-        self.full_text = f"{self.header_number} {self.header_text}"
+        self.full_text = f"{self.header_number} {self.text}"
         return self.full_text
 
     def get_indented_full_text(self, indent=2, indent_char=" ", begin_char="-"):
-        indent_str = indent_char * indent * (self.header_level - 1)
+        indent_str = indent_char * indent * (self.level - 1)
         self.indented_full_text = (
-            f"{indent_str}{begin_char} {self.header_number} {self.header_text}"
+            f"{indent_str}{begin_char} {self.header_number} {self.text}"
         )
         return self.indented_full_text
 
@@ -152,6 +147,34 @@ class SepNode(Node):
         self.type = "sep"
 
 
+class ElementNodelizer:
+    def __init__(self, element):
+        node = None
+        tag = element.name
+        class_str = " ".join(element.get("class", []))
+
+        if tag == "script":
+            node = JavascriptNode(element)
+        if tag in ["h1", "h2", "h3", "h4", "h5", "h6"]:
+            node = HeaderNode(element)
+        if class_str == "table" or tag == "table":
+            node = TableNode(element)
+        if class_str == "figure":
+            node = FigureNode(element)
+        if class_str == "sourceCode" or tag == "pre":
+            node = CodeNode(element)
+        if tag in ["p"]:
+            node = TextNode(element)
+        if tag in ["ul", "ol"]:
+            node = ListNode(element)
+        if tag in ["hr"]:
+            node = SepNode(element)
+        if class_str and class_str.startswith("section"):
+            node = SectionNode(element)
+
+        self.node = node
+
+
 class SpecHTMLNodelizer:
     def __init__(self, html_path):
         self.html_path = html_path
@@ -159,30 +182,38 @@ class SpecHTMLNodelizer:
             html_string = rf.read()
         self.soup = BeautifulSoup(html_string, "html.parser")
         self.nodes = []
+        self.section_node = None
 
-    def traverse_element(self, element):
+    def traverse_element(self, element, parent_section_node=None):
         children_l1 = element.find_all(recursive=False)
         for child in children_l1:
             tag = child.name
-            class_str = " ".join(child.get("class", []))
-            element_id = child.get("id", None)
+            # class_str = " ".join(child.get("class", []))
             # print(f"{tag}, class={class_str}, id={element_id}")
 
             node = ElementNodelizer(child).node
 
             if node:
                 self.nodes.append(node)
+                if parent_section_node:
+                    node.parent = parent_section_node
+                    parent_section_node.children.append(node)
+                if node.type == "section":
+                    self.traverse_element(child, parent_section_node=node)
             else:
                 if tag in ["div", "blockquote"]:
-                    self.traverse_element(child)
-                elif class_str and class_str.startswith("section"):
                     self.traverse_element(child)
                 else:
                     raise NotImplementedError
 
     def parse_html_to_nodes(self):
-        root_element = self.soup.find(id="MAIN")
-        self.traverse_element(root_element)
+        main_element = self.soup.find(id="MAIN")
+        main_node = SectionNode(main_element)
+        self.traverse_element(element=main_element, parent_section_node=main_node)
+        print(f"{len(self.nodes)} nodes parsed.")
+        for node in self.nodes:
+            if node.type == "section":
+                print(node.get_header_node().full_text)
 
     def run(self):
         self.parse_html_to_nodes()
