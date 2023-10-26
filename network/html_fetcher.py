9a102e72171734a8f8cad60e95bd9505a9bf9012
Hansimov <591172499@qq.com>
2023-10-26 13:04:16 +0800

    [Feature] UrlToPathConverter: Moduralize and support pattern match

--- network/html_fetcher.py
+++ network/html_fetcher.py
@@ -1,24 +1,48 @@
+import json
+import re
 from pathlib import Path
 from requests_html import HTMLSession
 from documents.ar5iv_html_nodelizer import Ar5ivHTMLNodelizer
 
 
-class HTMLFetcher:
+class UrlToPathConverter:
     def __init__(self, url):
         self.url = url
         self.html_dir = Path(__file__).parents[1] / "files" / "htmls"
         self.url_to_path()
 
-    def is_local_html_exists(self):
-        if self.output_path.exists():
-            return True
-
     def url_to_path(self):
-        # https://ar5iv.labs.arxiv.org/html/1810.04805
-        if "ar5iv.labs.arxiv" in self.url:
-            self.output_filename = self.url.split("/")[-1] + ".html"
+        if self.match_pattern("ar5iv"):
+            # https://ar5iv.labs.arxiv.org/html/1810.04805
+            match_res = self.match_pattern("ar5iv")
             self.output_dir = self.html_dir / "ar5iv"
+            self.output_filename = match_res.group(1) + ".html"
             self.output_path = self.output_dir / self.output_filename
+            self.requests_auth = None
+        elif self.match_pattern("docs.com"):
+            # https://docs.***.com/documents/**.html
+            match_res = self.match_pattern("docs.com")
+            self.output_dir = self.html_dir / match_res.group(1)
+            self.output_filename = match_res.group(2)
+            self.output_path = self.output_dir / self.output_filename
+            self.requests_auth = "ntlm"
+        else:
+            raise NotImplementedError(f"Not supported URL format: {self.url}")
+
+    def match_pattern(self, pattern):
+        patterns = {
+            "ar5iv": "ar5iv\.labs\.arxiv\.org/html/(.*)",
+            "docs.com": "docs\.(.*)\.com/documents/(.*\.html)",
+        }
+        return re.search(patterns[pattern], self.url)
+
+
+class HTMLFetcher:
+    def __init__(self, url):
+        self.url = url
+        url_to_path_converter = UrlToPathConverter(self.url)
+        self.output_path = url_to_path_converter.output_path
+
     def get(self, overwrite=True):
         print(f"Fetching {self.url}")
         if self.output_path.exists() and not overwrite:
