c2352a5b82f7f06fe7dde465b5ee6429f1098c34
Hansimov <591172499@qq.com>
2023-11-01 20:29:41 +0800

    [Feature] HTMLSearcherAPI: enable /search, and hide low-level api

--- apis/html_searcher_api.py
+++ apis/html_searcher_api.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from documents.htmls.html_nodelizer import HTMLNodelizer
 from documents.embeddings.embedder import EmbeddingEncoder, Reranker
+from documents.htmls.html_semantic_searcher import HTMLSemanticSearcher
 from documents.embeddings.node_embedder import NodeEmbedder
 from networks.html_fetcher import HTMLFetcher
 from typing import Union
@@ -38,6 +39,7 @@ class DocumentEntity:
         self.html_path = self.html_nodelizer.html_path
         self.nodes = self.html_nodelizer.nodes
         self.embeddings_df_pkl_path = self.node_embedder.embeddings_df_pkl_path
+        self.embeddings_df = self.node_embedder.embeddings_df
 
 
 class APIApp:
@@ -64,13 +66,13 @@ class APIApp:
             "html_path": html_fetcher.output_path,
         }
 
-    class EmbedFromUrlPostItem(BaseModel):
+    class EmbedHTMLFromUrlPostItem(BaseModel):
         url: str = Field(
             default=None,
             description="URL to fetch, nodelize and embed HTML",
         )
 
-    def embed_html_from_url(self, item: EmbedFromUrlPostItem):
+    def embed_html_from_url(self, item: EmbedHTMLFromUrlPostItem):
         url = item.url
         if url not in self.data_store:
             document_entity = DocumentEntity(url=url, app=self)
@@ -117,27 +119,50 @@ class APIApp:
             default=True,
             description="Sort the returned passages by score",
         )
-        rerank: bool = Field(
-            default=True,
-            description="Better results with more time consumption",
-        )
-        tokens_limit: int = Field(
+        max_tokens: int = Field(
             default=None,
             description="Max tokens for passages texts",
         )
-        passages_limit: int = Field(
-            default=None,
-            description="Max number of passages",
+        retrieve_top_k: int = Field(
+            default=50,
+            description="Max number of retrieved passages",
+        )
+        rerank_top_k: int = Field(
+            default=10,
+            description="Max number of reranked passages",
         )
 
     def search_passages_with_query(self, item: SearchPostItem):
-        pass
+        url = item.url
+        query = item.query
+        self.embed_html_from_url(item=APIApp.EmbedHTMLFromUrlPostItem(url=url))
+        self.html_semantic_searcher = HTMLSemanticSearcher(
+            url=url,
+            embedding_encoder=self.embedding_encoder,
+            reranker=self.reranker,
+            document_entity=self.data_store[url],
+        )
+        search_results = self.html_semantic_searcher.search(
+            query=query,
+            retrieve_top_k=item.retrieve_top_k,
+            rerank_top_k=item.rerank_top_k,
+        )
+        return {
+            "url": url,
+            "query": query,
+            "results": search_results,
+        }
 
     def setup_routes(self):
-        self.app.get(
-            "/url",
-            summary="Fetch HTML from URL",
-        )(self.fetch_html_from_url)
+        # self.app.get(
+        #     "/url",
+        #     summary="Fetch HTML from URL",
+        # )(self.fetch_html_from_url)
+
+        # self.app.post(
+        #     "/embed_url",
+        #     summary="Fetch, nodelize and embed HTML from URL",
+        # )(self.embed_html_from_url)
 
         self.app.post(
             "/embedding",
@@ -153,11 +178,6 @@ class APIApp:
             "/search",
             summary="Search passages from url with query",
         )(self.search_passages_with_query)
-
-        self.app.post(
-            "/embed_url",
-            summary="Fetch, nodelize and embed HTML from URL",
-        )(self.embed_html_from_url)
 
 
 app = APIApp().app
