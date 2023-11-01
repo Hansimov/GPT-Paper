20557ef40c31f5dc4b427ce0e6d6c03b6b9bb825
Hansimov <591172499@qq.com>
2023-11-01 14:52:26 +0800

    [Refactor] HTMLSearcherAPI: use OOP to re-write

--- apis/html_searcher_api.py
+++ apis/html_searcher_api.py
@@ -1,82 +1,80 @@
 import uvicorn
 from fastapi import FastAPI
-from typing import Union
-from documents.htmls.html_nodelizer import HTMLNodelizer
-from networks.html_fetcher import HTMLFetcher
-from documents.embeddings.embedder import EmbeddingEncoder, Reranker
-
 from pydantic import BaseModel
-
-app = FastAPI(docs_url="/")
-
-
-@app.get(
-    "/url",
-    summary="Fetch HTML from URL",
-)
-def fetch_html_from_url(url: str):
-    html_fetcher = HTMLFetcher(url)
-    html_fetcher.run()
-
-    return {
-        "url": url,
-        "html_path": html_fetcher.output_path,
-    }
+from documents.htmls.html_nodelizer import HTMLNodelizer
+from documents.embeddings.embedder import EmbeddingEncoder, Reranker
+from networks.html_fetcher import HTMLFetcher
 
 
-embedding_encoder = EmbeddingEncoder()
+class APIApp:
+    def __init__(self):
+        self.app = FastAPI(docs_url="/")
+        self.load_models()
+        self.setup_routes()
 
+    def load_models(self):
+        self.embedding_encoder = EmbeddingEncoder()
+        self.reranker = Reranker()
 
-class EmbeddingPostItem(BaseModel):
-    text: str = None
+    class EmbeddingPostItem(BaseModel):
+        text: str = None
 
+    class RerankPostItem(BaseModel):
+        query: str = None
+        passages: list[str] = None
+        sort: bool = True
 
-@app.post(
-    "/embedding",
-    summary="Encode text to embedding",
-)
-def encode_embedding(item: EmbeddingPostItem):
-    text = item.text
-    embedding = embedding_encoder.calc_embedding(text)
-    return {
-        "text": text,
-        "embedding": embedding.tolist(),
-    }
-
-
-reranker = Reranker()
-
-
-class RerankPostItem(BaseModel):
-    query: str = None
-    passages: list[str] = None
-    sort: bool = True
-
-
-@app.post(
-    "/rerank",
-    summary="Calculate rerank scores for list of passages with query",
-)
-def rerank(item: RerankPostItem):
-    pairs = [[item.query, passage] for passage in item.passages]
-    rerank_scores = reranker.compute_score(pairs).tolist()
-    rerank_scores_with_passages = [
-        {
-            "passage": passage,
-            "score": score,
+    def fetch_html_from_url(self, url: str):
+        html_fetcher = HTMLFetcher(url)
+        html_fetcher.run()
+        return {
+            "url": url,
+            "html_path": html_fetcher.output_path,
         }
-        for passage, score in zip(item.passages, rerank_scores)
-    ]
-    if item.sort:
-        rerank_scores_with_passages = sorted(
-            rerank_scores_with_passages, key=lambda x: x["score"], reverse=True
-        )
 
-    return {
-        "query": item.query,
-        "rerank_scores": rerank_scores_with_passages,
-    }
+    def calculate_embedding(self, item: EmbeddingPostItem):
+        text = item.text
+        embedding = self.embedding_encoder.calc_embedding(text)
+        return {
+            "text": text,
+            "embedding": embedding.tolist(),
+        }
 
+    def rerank_passages_with_query(self, item: RerankPostItem):
+        pairs = [[item.query, passage] for passage in item.passages]
+        rerank_scores = self.reranker.compute_score(pairs).tolist()
+        rerank_scores_with_passages = [
+            {
+                "passage": passage,
+                "score": score,
+            }
+            for passage, score in zip(item.passages, rerank_scores)
+        ]
+        if item.sort:
+            rerank_scores_with_passages = sorted(
+                rerank_scores_with_passages, key=lambda x: x["score"], reverse=True
+            )
+        return {
+            "query": item.query,
+            "rerank_scores": rerank_scores_with_passages,
+        }
+
+    def setup_routes(self):
+        self.app.get(
+            "/url",
+            summary="Fetch HTML from URL",
+        )(self.fetch_html_from_url)
+        self.app.post(
+            "/embedding",
+            summary="Calculate embedding for text",
+        )(self.calculate_embedding)
+        self.app.post(
+            "/rerank",
+            summary="Calculate rerank scores for list of passages with query",
+        )(self.rerank_passages_with_query)
+
+
+app = APIApp().app
 
 if __name__ == "__main__":
     uvicorn.run("__main__:app", host="0.0.0.0", port=12344, reload=True)
