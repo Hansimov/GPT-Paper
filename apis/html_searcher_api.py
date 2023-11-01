14c43b18227d717a1e9fb8aec3e295505d9f09e1
Hansimov <591172499@qq.com>
2023-11-01 15:28:43 +0800

    [Enhance] Documents Retriever APIs with more comments and schema

--- apis/html_searcher_api.py
+++ apis/html_searcher_api.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from documents.htmls.html_nodelizer import HTMLNodelizer
 from documents.embeddings.embedder import EmbeddingEncoder, Reranker
 from networks.html_fetcher import HTMLFetcher
@@ -8,21 +8,18 @@ from networks.html_fetcher import HTMLFetcher
 
 class APIApp:
     def __init__(self):
-        self.app = FastAPI(docs_url="/")
-        self.load_models()
+        self.app = FastAPI(
+            docs_url="/",
+            title="Documents Retriever API",
+            version="1.0",
+        )
+        self.load_cache()
         self.setup_routes()
 
-    def load_models(self):
+    def load_cache(self):
         self.embedding_encoder = EmbeddingEncoder()
         self.reranker = Reranker()
-
-    class EmbeddingPostItem(BaseModel):
-        text: str = None
-
-    class RerankPostItem(BaseModel):
-        query: str = None
-        passages: list[str] = None
-        sort: bool = True
+        self.html_nodelizer_cache = {}
 
     def fetch_html_from_url(self, url: str):
         html_fetcher = HTMLFetcher(url)
@@ -32,6 +29,16 @@ class APIApp:
             "html_path": html_fetcher.output_path,
         }
 
+    class EmbeddingPostItem(BaseModel):
+        text: str = None
+
+    class EmbeddingPostResponseItem(BaseModel):
+        text: str = None
+        embedding: list[float] = Field(
+            default=None,
+            description="`list[float]`: Embeddings of the text",
+        )
+
     def calculate_embedding(self, item: EmbeddingPostItem):
         text = item.text
         embedding = self.embedding_encoder.calc_embedding(text)
@@ -39,6 +46,25 @@ class APIApp:
             "text": text,
             "embedding": embedding.tolist(),
         }
+
+    class RerankPostItem(BaseModel):
+        query: str = None
+        passages: list[str] = Field(
+            default=None,
+            description="`list[str]`: Passages to rerank.",
+        )
+        sort: bool = Field(
+            default=True,
+            description="Sort the reranked passages by score",
+        )
+
+    class RerankPostResponseItem(BaseModel):
+        query: str = None
+        rerank_scores: list[dict] = Field(
+            default=None,
+            description="`list[dict]`: Rerank scores of the passages. "
+            'Each dict item: `{"passage": ..., "score": ...}`',
+        )
 
     def rerank_passages_with_query(self, item: RerankPostItem):
         pairs = [[item.query, passage] for passage in item.passages]
@@ -59,6 +85,29 @@ class APIApp:
             "rerank_scores": rerank_scores_with_passages,
         }
 
+    class RetrievePostItem(BaseModel):
+        query: str = None
+        url: str = None
+        sort: bool = Field(
+            default=True,
+            description="Sort the returned passages by score",
+        )
+        rerank: bool = Field(
+            default=True,
+            description="Better results with more time consumption",
+        )
+        tokens_limit: int = Field(
+            default=None,
+            description="Max tokens for passages texts",
+        )
+        passages_limit: int = Field(
+            default=None,
+            description="Max number of passages",
+        )
+
+    def retrieve_passages_with_query(self, item: RetrievePostItem):
+        pass
+
     def setup_routes(self):
         self.app.get(
             "/url",
@@ -67,11 +116,17 @@ class APIApp:
         self.app.post(
             "/embedding",
             summary="Calculate embedding for text",
+            response_model=self.EmbeddingPostResponseItem,
         )(self.calculate_embedding)
         self.app.post(
             "/rerank",
             summary="Calculate rerank scores for list of passages with query",
+            response_model=self.RerankPostResponseItem,
         )(self.rerank_passages_with_query)
+        self.app.post(
+            "/retrieve",
+            summary="Retrieve passages from url with query",
+        )(self.retrieve_passages_with_query)
 
 
 app = APIApp().app
